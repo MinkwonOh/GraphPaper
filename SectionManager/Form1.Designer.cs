8987ef079395c120319774b3a9eda62a4db9e0d5
MinkwonOh <omk0928@corun.co.kr>
2024-04-23 16:31:30 +0900

    add project with ignore file

---
+++ SectionManager/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace SectionManager {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pnl_background = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnl_background
+            // 
+            this.pnl_background.BackColor = System.Drawing.Color.White;
+            this.pnl_background.Location = new System.Drawing.Point(20, 50);
+            this.pnl_background.Name = "pnl_background";
+            this.pnl_background.Size = new System.Drawing.Size(766, 565);
+            this.pnl_background.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 1074);
+            this.Controls.Add(this.pnl_background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_background;
+    }
+}
+
